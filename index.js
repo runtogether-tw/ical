923456e4b345683caf19e85a26e4ed55364e33d1
Chris Cheng <chris38c28@hotmail.com>
2023-05-17 20:50:20 +0800

    SGDQ2023

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const ical = require('ical-generator');
 const fs = require('fs');
 
-const EVENT = 41;
-const EVENT_NAME = 'AGDQ2023';
+const EVENT = 43;
+const EVENT_NAME = 'SGDQ2023';
 
 async function genICal() {
     const schedule = await axios.get(`https://gamesdonequick.com/tracker/search/?type=run&event=${EVENT}`).then(res => res.data);
